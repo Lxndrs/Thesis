c6e9b87149dd8bcab45e3ae5d132c13f783f88f6
Alejandro Tarango <j.tarango@crya.unam.mx>
2018-07-16 14:39:52 -0600

    Final version of wind-fits.tab Uncertainties included for subsamples
    - Inserted errorbars into plot-wind-fits.pdf

--- Programs/correct_for_distance.py
+++ Programs/correct_for_distance.py
@@ -16,14 +16,38 @@ sources = ["168-328", "169-338", "177-341", "180-331", "LV2", "LV2b", "LV3", "LV
 d_prime = {"168-328":6.8, "169-338":16.4, "177-341":25.6, "180-331":25.1, "LV2":7.8, "LV2b":7.2, "LV3":6.9, "LV4":6.2, "LV5":9.6}
 r14_HA = {"168-328":2.8, "169-338":2.8, "177-341":20.4, "180-331":12.2, "LV2":7.9, "LV2b":2.5, "LV3":5.0, "LV4":3.5, "LV5":6.3}
 dr14_HA = {"168-328":0.3, "169-338":0.3, "177-341":1.6, "180-331":1.2, "LV2":0.3, "LV2b":0.6, "LV3":0.6, "LV4":0.3, "LV5":0.6}
+N6_HA = {"168-328":4.00, "169-338":1.40, "177-341":0.41, "180-331":0.48, "LV2":2.53, "LV2b":4.13, "LV3":3.11, "LV4":4.13, "LV5":2.33}
+dN6_HA_P = {"168-328":2.11, "169-338":0.91, "177-341":0.11, "180-331":0.25, "LV2":1.06, "LV2b":1.85, "LV3":1.48, "LV4":1.97, "LV5":1.13}
+dN6_HA_m = {"168-328":0.02, "169-338":0.05, "177-341":0.03, "180-331":0.05, "LV2":0.17, "LV2b":0.25, "LV3":0.44, "LV4":0.34, "LV5":0.33}
 d_ori_HA = 460
 d_ori_new = 414
+Delta_d_ori_new = 6.8
 
 # Compute new data
+savedata = {"r14":"", "dr14":"", "N6":"", "dN6+":"", "dN6-":""}
 r14_new = {}
+dr14_new = {}
+N6_new = {}
+dN6_P_new = {}
+dN6_m_new = {}
 for source in sources:
-    r14_new[source] = "{:.1f}".format(r14_HA[source]*d_ori_new/d_ori_HA)
+    r14 = r14_HA[source]*d_ori_new/d_ori_HA
+    dr14 = r14*(Delta_d_ori_new/d_ori_new + dr14_HA[source]/r14_HA[source])
+    N6 = N6_HA[source]*np.sqrt(r14_HA[source]/r14)
+    N6_new[source] = "{:.2f}".format(N6)
+    dN6_P = N6*(dN6_HA_P[source]/N6_HA[source] + 0.5*dr14/r14 + 0.5*dr14_HA[source]/r14_HA[source])
+    dN6_m = N6*(dN6_HA_m[source]/N6_HA[source] + 0.5*dr14/r14 + 0.5*dr14_HA[source]/r14_HA[source])
+    r14_new[source] = "{:.1f}".format(r14)
+    dr14_new[source] = "{:.1f}".format(dr14)
+    dN6_P_new[source] = "{:.2f}".format(dN6_P)
+    dN6_m_new[source] = "{:.2f}".format(dN6_m)
+
+savedata["r14"] = r14_new
+savedata["dr14"] = dr14_new
+savedata["N6"] = N6_new
+savedata["dN6+"] = dN6_P_new
+savedata["dN6-"] = dN6_m_new
 
 savefile = "distances_corrected.json"
 with open(savefile, 'w') as f:
-    json.dump(r14_new, f, indent=2)
+    json.dump(savedata, f, indent=2)
--- Programs/plot-wind-fits-mod.py
+++ Programs/plot-wind-fits-mod.py
@@ -1,7 +1,156 @@
+
 """Plot data from table of pressures and fluxes from proplyd arc fits
    Original: William Henney.
    Modified Version: Jorge A. Tarango-Yong
 """
+
+from astropy.table import Table
+from matplotlib import pyplot as plt
+import json
+import numpy as np
+import seaborn as sns
+
+def split_list(tab):
+    """
+    Some elements in the input table include uncertainties in the form 'x +/- dx'. 
+    So is neccesary to separate them into two arrays.
+    Input: Element from astropy table.
+    Output:
+        x: Measurements
+        dx: Uncertainties
+    """
+    x = []
+    dx = []
+    for t in tab:
+        x.append(float(t.split("+/-")[0]))
+        dx.append(float(t.split("+/-")[-1]))
+    return x, dx
+
+# Useful Physical and Astronomical constants.
+AU = 1.49597870691e13
+PC = 3.085677582e18
+d_Orion = 414.0 # Use Menten et al. (2007) Measurement
+k_Boltzmann = 1.3806503e-16
+cos80 = 0.173648177667
+
+# Read parameters table
+tab = Table.read('../wind-fits.tab', format='ascii.tab')
+sources = sorted(set(tab['Fuente']))
+n = len(sources)
+colors = sns.color_palette('Set1', n)
+sns.set_style("whitegrid")
+# Physical separations in parsec
+D, Delta_D = split_list(tab["D(as)"])
+D_pc = np.array(D)*d_Orion*AU/PC
+Delta_D_pc = D_pc*(np.array(Delta_D)/np.array(D))
+ 
+#split F(star), P(wind) and F(ph)/F(*)+  columns
+Fs, dFs = split_list(tab["F(star)"])
+Fs, dFs = np.array(Fs), np.array(dFs)
+Pw, dPw = split_list(tab["P(wind)"])
+Fr, dFr = split_list(tab["F(ph)/F(*)+"])
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+ax1.loglog(D_pc, Fs, c='k', alpha=0.1, lw=10, label='')
+ax2.loglog(D_pc, np.array(Pw)/k_Boltzmann, c='k', alpha=0.1, lw=10, label='')
+mm = (np.array(Fr) > 2./3) & (np.array(Fr) < 1.5) # Subsamples where photoionization balance is accomplished
+nn = (np.array(Fr) > 1./3) & (np.array(Fr) < 3.) # Subsamples where photoionization balance is at least weakly accomplished.
+used = (np.array(Fr) > 0.1) & (np.array(Fr) < 10.) # Subsamples where photoionization balance is at least poorly accomplished
+#out_colnames = ['Source' ,'D prime', 'R0/D prime', 'Rc/R0 prime full',
+#                'Rc/R0 prime select', 'beta', 'xi', 'inc', 'D', 'R0/D']
+
+#out_formats = {
+#    'D prime': '%.1f',
+#    'R0/D prime':         '%.4f',
+#    'Rc/R0 prime full':   '%.4f',
+#    'Rc/R0 prime select': '%.4f',
+#    'beta':               '%.4f',
+#    'xi':                 '%.4f',
+#    'inc':                '%.4f',
+#    'D':                  '%.4f',
+#    'R0/D':               '%.4f',
+#
+#
+#out_rows = []
+
+def var_range(x, dx):
+    return x, dx
+
+
+for source, color in zip(sources, colors):
+    m = tab['Fuente'] == source
+    Dprime = tab["D'(as)"][m][0] * d_Orion*AU/PC
+    Fp, dFp_plus = split_list(tab['F(photo)+'])
+    Fp, dFp_minus = split_list(tab['F(photo)-'])
+    Fp = np.array(Fp)
+    dFp = np.array([dFp_minus, dFp_plus])
+    Pi, dPi_plus = split_list(tab["P(in)+"])
+    Pi, dPi_minus = split_list(tab["P(in)-"])
+    dPi = np.array([dPi_minus, dPi_plus])
+    Pi = np.array(Pi)
+    # Get data from variational fits
+#    combined_file = '../saves/LV-bowshocks-xyfancy-variations-{}.save'.format(source)
+#    vardata = json.load(open(combined_file))
+    # Combine with data from org table to fill in a new output table (I don't need this anymore)
+#    Rcp_select = tab['\Pi\''][m & mm]
+#    Rcp_full = np.array(vardata['Rc'])/np.array(vardata['R0'])
+#    beta = tab[r'\beta'][m & mm]
+#    xi = tab['xi'][m & mm]
+#    inc = tab['i (deg)'][m & mm]
+#    DD = tab['D(as)'][m & mm] * d_Orion*AU/PC
+#    R0_D = tab['q'][m & mm]
+
+# Actually we don't need the table work since i already did it
+#    out_rows.append({
+#        'Source': source,
+#        'D prime': tab['D\'(as)'][m][0],
+#        'R0/D prime':         var_range(np.mean(vardata['R0']), np.std(vardata['R0'])),
+#        'Rc/R0 prime full':    var_range(np.mean(Rcp_full), np.std(Rcp_full)), 
+#        'Rc/R0 prime select':  var_range(np.mean(Rcp_select), np.std(Rcp_select)),
+#        'beta':               var_range(np.mean(beta), np.std(beta)), 
+#        'xi':                 var_range(np.min(xi), np.max(xi)),
+#        'inc':                var_range(np.mean(inc), np.std(inc)),
+#        'D':                  var_range(np.mean(DD), np.std(DD)),
+#        'R0/D':               var_range(np.mean(R0_D), np.std(R0_D)),
+#    })
+                   
+    ax1.loglog([Dprime, Dprime/cos80], [Fp[m], Fp[m]], ':', c=color, alpha=0.4, label='')
+    ax1.loglog(D_pc[m & used], Fp[m & used], '.-', c=color, alpha=0.4, label='')
+    ax1.errorbar(D_pc[m & used], Fp[m & used], xerr=Delta_D_pc[m & used],yerr=dFp[:, m & used], c=color, alpha=0.4)
+    ax1.loglog(D_pc[m & mm], Fp[m & mm],
+               'o-', lw=3, c=color, label=source)
+    ax1.errorbar(D_pc[m & mm], Fp[m & mm], xerr=Delta_D_pc[m & mm], yerr=dFp[:, m & mm], c=color)
+    ax2.loglog(D_pc[m & used], Pi[m & used]/k_Boltzmann,
+               '.-', c=color, alpha=0.4, label='')
+    ax2.errorbar(D_pc[m & used], Pi[m & used]/k_Boltzmann, xerr=Delta_D_pc[m & used], yerr=dPi[:, m & used]/k_Boltzmann, c=color, alpha=0.4)
+    ax2.loglog(D_pc[m & mm], Pi[m & mm]/k_Boltzmann,
+               'o-', c=color, lw=3, label=source)
+    ax2.errorbar(D_pc[m & mm], Pi[m & mm]/k_Boltzmann, xerr=Delta_D_pc[m & mm], yerr=dPi[:, m & mm]/k_Boltzmann, c=color)
+    # Now plot the subsamples which accomplish weakly photoionization balance.
+    ax1.loglog(D_pc[m & (nn & ~mm)], Fp[m & (nn & ~mm)],
+               'o-', lw=0.5, c=color, label='', alpha=0.4)
+    ax1.errorbar(D_pc[m & (nn & ~mm)], Fp[m & (nn & ~mm)], xerr = Delta_D_pc[m & (nn & ~mm)], yerr=dFp[:, m & (nn & ~mm)], c=color, alpha=0.4)
+    ax2.loglog(D_pc[m & (nn & ~mm)], Pi[m & (nn & ~mm)]/k_Boltzmann,
+               'o-', c=color, lw=0.5, label='', alpha=0.4)
+    ax2.errorbar(D_pc[m & (nn & ~mm)], Pi[m & (nn & ~mm)]/k_Boltzmann, xerr=Delta_D_pc[m & (nn & ~mm)], yerr=dPi[:, m & (nn & ~mm)]/k_Boltzmann, c=color, alpha=0.4)
+ax2.legend(ncol=2, loc='lower left')
+ax2.set_xlim(0.008, 0.3)
+ax2.set_ylim(1e7, 4e9)
+ax1.set_ylim(2e12, 3e14)
+ax1.set_ylabel(r'Ionizing Flux, $\mathrm{cm^{-2}\ s^{-1}}$')
+ax2.set_ylabel(r'Stagnation Pressure: $P/k$, $\mathrm{cm^{-3}\ K}$')
+ax2.set_xlabel('Distance, parsec')
+fig.set_size_inches(5, 8)
+fig.tight_layout()
+fig.savefig('../Figures/plot-wind-fits.pdf')
+
+#out_tab = Table(names=out_colnames, rows=out_rows)
+
+#print(out_tab.pprint(max_width=-1, max_lines=-1))
+
+#out_tab.write('../arc-fit-table-for-paper.tab',
+#              format='ascii.fixed_width', formats=out_formats)
+
+
 """Original Version:
 from astropy.table import Table
 from matplotlib import pyplot as plt
@@ -107,118 +256,3 @@ out_tab.write('arc-fit-table-for-paper.tab',
 
 
 """
-
-from astropy.table import Table
-from matplotlib import pyplot as plt
-import json
-import numpy as np
-import seaborn as sns
-
-
-# Useful Physical and Astronomical constants.
-AU = 1.49597870691e13
-PC = 3.085677582e18
-d_Orion = 414.0 # Use Menten et al. (2007) Measurement
-k_Boltzmann = 1.3806503e-16
-cos80 = 0.173648177667
-
-# Read parameters table
-tab = Table.read('../wind-fits.tab', format='ascii.tab')
-sources = sorted(set(tab['Fuente']))
-n = len(sources)
-colors = sns.color_palette('Set1', n)
-
-# Physical separations in parsec
-D = tab['D(as)'] * d_Orion*AU/PC
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-ax1.loglog(D, tab['F(star)'], c='k', alpha=0.1, lw=10, label='')
-ax2.loglog(D, tab['P(wind)']/k_Boltzmann, c='k', alpha=0.1, lw=10, label='')
-mm = (tab['F(ph)/F(*)'] > 0.707) & (tab['F(ph)/F(*)'] < 1.414) # Subsamples where photoionization balance is accomplished
-nn = (tab['F(ph)/F(*)'] > 0.33) & (tab['F(ph)/F(*)'] < 3.) # Subsamples where photoionization balance is  weakly accomplished.
-out_colnames = ['Source' ,'D prime', 'R0/D prime', 'Rc/R0 prime full',
-                'Rc/R0 prime select', 'beta', 'xi', 'inc', 'D', 'R0/D']
-
-out_formats = {
-    'D prime': '%.1f',
-    'R0/D prime':         '%.4f',
-    'Rc/R0 prime full':   '%.4f',
-    'Rc/R0 prime select': '%.4f',
-    'beta':               '%.4f',
-    'xi':                 '%.4f',
-    'inc':                '%.4f',
-    'D':                  '%.4f',
-    'R0/D':               '%.4f',
-}
-
-out_rows = []
-
-def var_range(x, dx):
-    return x, dx
-
-
-for source, color in zip(sources, colors):
-    m = tab['Fuente'] == source
-    Dprime = tab['D\'(as)'][m][0] * d_Orion*AU/PC
-    F = tab['F(photo)'][m][0]
-
-    # Get data from variational fits
-    combined_file = '../saves/LV-bowshocks-xyfancy-variations-{}.save'.format(source)
-    vardata = json.load(open(combined_file))
-    # Combine with data from org table to fill in a new output table 
-    Rcp_select = tab['\Pi\''][m & mm]
-    Rcp_full = np.array(vardata['Rc'])/np.array(vardata['R0'])
-    beta = tab[r'\beta'][m & mm]
-    xi = tab['xi'][m & mm]
-    inc = tab['i (deg)'][m & mm]
-    DD = tab['D(as)'][m & mm] * d_Orion*AU/PC
-    R0_D = tab['q'][m & mm]
-
-# Actually we don't need the table work since i already did it
-#    out_rows.append({
-#        'Source': source,
-#        'D prime': tab['D\'(as)'][m][0],
-#        'R0/D prime':         var_range(np.mean(vardata['R0']), np.std(vardata['R0'])),
-#        'Rc/R0 prime full':    var_range(np.mean(Rcp_full), np.std(Rcp_full)), 
-#        'Rc/R0 prime select':  var_range(np.mean(Rcp_select), np.std(Rcp_select)),
-#        'beta':               var_range(np.mean(beta), np.std(beta)), 
-#        'xi':                 var_range(np.min(xi), np.max(xi)),
-#        'inc':                var_range(np.mean(inc), np.std(inc)),
-#        'D':                  var_range(np.mean(DD), np.std(DD)),
-#        'R0/D':               var_range(np.mean(R0_D), np.std(R0_D)),
-#    })
-                   
-    ax1.loglog([Dprime, Dprime/cos80], [F, F], ':', c=color, alpha=0.4, label='')
-    ax1.loglog(D[m], tab['F(photo)'][m],
-               '-', c=color, alpha=0.4, label='')
-    ax1.loglog(D[m & mm], tab['F(photo)'][m & mm],
-               'o-', lw=3, c=color, label=source)
-    ax2.loglog(D[m], tab['P(in)'][m]/k_Boltzmann,
-               '-', c=color, alpha=0.4, label='')
-    ax2.loglog(D[m & mm], tab['P(in)'][m & mm]/k_Boltzmann,
-               'o-', c=color, lw=3, label=source)
-    # Now plot the subsamples which accomplish weakly photoionization balance.
-    ax1.loglog(D[m], tab['F(photo)'][m],
-               '-', c=color, alpha=0.4, label='')
-    ax1.loglog(D[m & (nn & ~mm)], tab['F(photo)'][m & (nn & ~mm)],
-               'o-', lw=0.5, c=color, label='', alpha=0.4)
-    ax2.loglog(D[m], tab['P(in)'][m]/k_Boltzmann,
-               '-', c=color, alpha=0.4, label='')
-    ax2.loglog(D[m & (nn & ~mm)], tab['P(in)'][m & (nn & ~mm)]/k_Boltzmann,
-               'o-', c=color, lw=0.5, label='', alpha=0.4)
-ax2.legend(ncol=2, loc='lower left')
-ax2.set_xlim(0.008, 0.3)
-ax2.set_ylim(1e7, 4e9)
-ax1.set_ylim(2e12, 3e14)
-ax1.set_ylabel(r'Ionizing Flux, $\mathrm{cm^{-2}\ s^{-1}}$')
-ax2.set_ylabel(r'Stagnation Pressure: $P/k$, $\mathrm{cm^{-3}\ K}$')
-ax2.set_xlabel('Distance, parsec')
-fig.set_size_inches(5, 8)
-fig.tight_layout()
-fig.savefig('../Figures/plot-wind-fits.pdf')
-
-#out_tab = Table(names=out_colnames, rows=out_rows)
-
-#print(out_tab.pprint(max_width=-1, max_lines=-1))
-
-#out_tab.write('../arc-fit-table-for-paper.tab',
-#              format='ascii.fixed_width', formats=out_formats)
